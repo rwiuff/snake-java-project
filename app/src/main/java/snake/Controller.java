b5f965e115aa35360c8eb2c0af2418a9215e2ef0
Niklas <s235235@student.dtu.dk>
2024-01-03 11:39:56 +0100

    Inititial controller, change direction method

--- app/src/main/java/snake/Controller.java
+++ app/src/main/java/snake/Controller.java
@@ -1,6 +1,15 @@
 package snake;
 
-public class Controller {
 
+public class Controller {
+    private Board board;
+    
+
+    public Controller (Board board) {
+        this.board =board;
+    }
+    public void changeDir (int dir) {
+        this.board.getSnake().getHead().setDir(dir);
+    }
     
 }
