183f0f989c68269d7b521dedd7f2679260a1bea2
Rasmus Wiuff <rwiuff@gmail.com>
2024-01-03 13:05:19 +0100

    arguments works

--- app/src/main/java/snake/Main.java
+++ app/src/main/java/snake/Main.java
@@ -1,40 +1,48 @@
 package snake;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
+
+    public static String[] dimensions;
+
     public static void main(String[] args) {
+        System.out.println("Width: " + args[0]);
+        System.out.println("Height: " + args[1]);
+        dimensions = args;
         launch(args);
     }
 
-    // @Override
-    // public void start(Stage primaryStage) throws Exception {
-    //     // TODO Auto-generated method stub
-        
-    // }
-
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Welcome to snek");
+        int width = Integer.parseInt(dimensions[0]);
+        int height = Integer.parseInt(dimensions[1]);
+        GridPane gridPane = new GridPane();
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                Rectangle r = new Rectangle();
+                // r.setX(50);
+                // r.setY(50);
+                r.setWidth(50);
+                r.setHeight(50);
+                // r.setArcWidth(20);
+                // r.setArcHeight(20);
+                r.setFill(Color.OLIVE);
+                r.setStroke(Color.WHEAT);
+                gridPane.add(r, i, j);
             }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        }
+        Scene scene = new Scene(gridPane, 600, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
+
 }
