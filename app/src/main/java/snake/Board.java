9235f4436f6138cec7d10c18af149573d31dc4b3
Niklas <s235235@student.dtu.dk>
2024-01-03 13:57:09 +0100

    Add apple placement. Add placeNew to all spaces changed return type to boolean for collision (boolean used to excute extra methods)

--- app/src/main/java/snake/Board.java
+++ app/src/main/java/snake/Board.java
@@ -1,27 +1,39 @@
 package snake;
 
 import java.awt.Point;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
     private Space[][] board;
     private SnakeObject snake;
-    private Apple apple;
-    private HashSet<Point> emptySpaces = new HashSet<Point>();
-
+    private ArrayList<Point> emptySpaces = new ArrayList<Point>();
+    private Random random=new Random();
 
     public Board(int n, int m) {
         this.board = new Space[n][m];
         this.snake = new SnakeObject(n, m);
-        this.apple = new Apple(1, 2);
+        this.board[this.snake.getHead().getX()][this.snake.getHead().getY()]=this.snake.getHead();
+        this.board[this.snake.getBody().get(0).getX()][this.snake.getBody().get(0).getY()]=snake.getBody().get(0);
+        for (int row =0; row<n;row++) {
+            for (int column=0; column<m;column++) {
+                if (this.board[row][column]==null) {
+                    emptySpaces.add(new Point (row,column));
+                }
+            }
+        }
+        placeApple();
         update();
+
     }
 
     public void update(){
         this.board[snake.getBody().get(0).getX()][snake.getBody().get(0).getY()] = null;
         snake.snakeMove();
         try {
-            this.board[snake.getHead().getX()][snake.getHead().getY()].collision(snake);
+            if (this.board[snake.getHead().getX()][snake.getHead().getY()].collision(snake)) {
+                this.board[snake.getHead().getX()][snake.getHead().getY()].placeNew(this.board,this.emptySpaces);
+            };
             
         } catch (Exception e) {
             // pass
@@ -37,7 +49,14 @@ public class Board {
         }
     }
 
-    public static void placeApple(){
+    public void placeApple(){
+        int index=random.nextInt(emptySpaces.size());
+        int x =(int) this.emptySpaces.get(index).getX();
+        int y= (int) this.emptySpaces.get(index).getY();
+        this.board[x][y]=new Apple(x,y);
+
+        
+
 
     }
     
