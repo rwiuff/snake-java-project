78cc028ec417150e8dd631974d424194ca1854a5
Sophie <s235216@student.dtu.dk>
2024-01-04 11:52:27 +0100

    merge conflicts

--- app/src/main/java/snake/Board.java
+++ app/src/main/java/snake/Board.java
@@ -3,7 +3,6 @@ package snake;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Random;
-import javafx.scene.paint.Color;
 
 public class Board {
     public Space[][] board;
@@ -51,19 +50,19 @@ public class Board {
     public void update() {
         this.board[snake.getBody().get(0).getX()][snake.getBody().get(0).getY()] = null;
         SnakeSegment tail = this.snake.getBody().get(this.getSnake().getLength() - 2); // -2 as length includes head,
-        Point tailPlace = new Point(tail.getX(),tail.getY()); 
-        int tempLength=this.snake.getLength();                                                   // while body list does not
+        Point tailPlace = new Point(tail.getX(), tail.getY());
+        int tempLength = this.snake.getLength(); // while body list does not
         this.emptySpaces.add(tailPlace); // objects can be placed
         snake.snakeMove();
         this.emptySpaces.remove(new Point(this.snake.getHead().getX(), this.snake.getHead().getY()));
 
         try {
             if (this.board[snake.getHead().getX()][snake.getHead().getY()].collision(snake)) {
-                if (this.snake.getLength()!=tempLength) { 
+                if (this.snake.getLength() != tempLength) {
                     this.emptySpaces.remove(tailPlace);
                 }
                 this.board[snake.getHead().getX()][snake.getHead().getY()].placeNew(this.board, this.emptySpaces);
-                
+
             }
             ;
 
@@ -91,6 +90,7 @@ public class Board {
     public SnakeObject getSnake() {
         return this.snake;
     }
+
     public Space[][] getBoard() {
         return this.board;
     }
--- app/src/main/java/snake/Main.java
+++ app/src/main/java/snake/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public static String[] dimensions;
 
     public static void main(String[] args) {
-        System.out.println(args.length);
+        // System.out.println(args.length);
         dimensions = args.length < 2 ? new String[] { "20", "20" } : args;
         System.out.println("Width: " + dimensions[0]);
         System.out.println("Height: " + dimensions[1]);
@@ -30,20 +30,20 @@ public class Main extends Application {
         boardcontroller.setDimensions(width, height);
         boardcontroller.initialise();
         boardcontroller.drawBoard();
-        
+
         primaryStage.setTitle("Welcome to snek");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-        double x = primaryStage.getX();
-        double y = primaryStage.getY();
-        double xD = primaryStage.getWidth();
-        double yD = primaryStage.getHeight();
+        // double x = primaryStage.getX();
+        // double y = primaryStage.getY();
+        // double xD = primaryStage.getWidth();
+        // double yD = primaryStage.getHeight();
         primaryStage.sizeToScene();
         primaryStage.centerOnScreen();
         // primaryStage.setX(x + ((xD - primaryStage.getWidth()) / 2));
         // primaryStage.setY(y + ((yD - primaryStage.getHeight()) / 2));
-        System.out.println("Breakpoint");
+        // System.out.println("Breakpoint");
     }
 
 }
