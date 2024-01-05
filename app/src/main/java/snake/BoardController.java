190859727c519c8dd724e182544978a55b2a6aff
Rasmus Wiuff <rwiuff@gmail.com>
2024-01-05 10:21:49 +0100

    began dynamic update

--- app/src/main/java/snake/BoardController.java
+++ app/src/main/java/snake/BoardController.java
@@ -3,12 +3,16 @@ package snake;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import java.util.HashMap;
+import java.util.Set;
+import java.awt.Point;
 
 public class BoardController {
     // @FXML
@@ -19,8 +23,12 @@ public class BoardController {
     private int width;
     private int height;
     private Board board;
+    private HashMap<String,Point> updateFields;
+
+    private Scene scene;
 
     public void run(Scene scene) {
+        this.scene = scene;
         this.board = new Board(width, height);
         drawBoard();
         scene.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>() {
@@ -46,10 +54,18 @@ public class BoardController {
                         break;
                 }
                 board.getSnake().getHead().setDir(direction);
-                board.update();
-                drawBoard();
+                updateFields = board.update();
+                reDrawBoard(updateFields);
             }
         });
+    }
+
+    protected void reDrawBoard(HashMap<String,Point> updateFields) {
+        Node grid = borderPane.lookup("gridPane");
+        Set<String> keys = updateFields.keySet();
+        for (String key : keys) {
+            Point updateObject = updateFields.get(key);
+        }
     }
 
     public void setDimensions(int width, int height) {
@@ -59,6 +75,7 @@ public class BoardController {
 
     public void drawBoard() {
         GridPane gridPane = new GridPane();
+        gridPane.setId("gridPane");
         Space[][] spaces = board.getBoard();
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
@@ -71,6 +88,7 @@ public class BoardController {
                     rectangle.setFill(Color.OLIVE);
                 }
                 rectangle.setStroke(Color.WHEAT);
+                rectangle.setId(i + ";" + j);
                 gridPane.add(rectangle, i, j);
             }
         }
