b7ede24714283b606b9cd21bb85df4c007163863
s235235 <s235235@dtu.dk>
2024-01-05 12:07:22 +0100

    try again to update snake extension

--- app/src/main/java/snake/SnakeObject.java
+++ app/src/main/java/snake/SnakeObject.java
@@ -7,16 +7,17 @@ public class SnakeObject{
     private ArrayList<SnakeSegment> body = new ArrayList<SnakeSegment>();
     private int length;
     private int boardSize;
-     
+    private SnakeSegment ghostTail;
     public SnakeObject(int vSize, int hSize) {
         this.head = new SnakeHead(vSize / 2, hSize / 2, hSize, vSize);
         this.body.add(new SnakeSegment(vSize / 2, (hSize / 2)+1));
-                this.length = 2;
+        this.ghostTail= new SnakeSegment(vSize/2,hSize/2+2);
+        this.length = 2;
         this.boardSize = hSize*vSize;
     }
 
     public void extend() {
-        this.body.add(0, new SnakeSegment(body.get(0).getX(), body.get(0).getY()));
+        this.body.add(0, this.ghostTail);
         this.length++;
         if (boardSize == length) {
             System.out.println("You win!");
@@ -26,7 +27,7 @@ public class SnakeObject{
     public void snakeMove(){
         body.add(new SnakeSegment(head.getX(), head.getY()));
         head.move();
-        body.remove(0);
+        this.ghostTail=body.remove(0);
     }
 
     public SnakeHead getHead(){
@@ -42,5 +43,8 @@ public class SnakeObject{
     }
     public SnakeSegment getTail(){
         return this.body.get(0);
+    }
+    public SnakeSegment getGhostTail() {
+        return this.ghostTail;
     }
     }
