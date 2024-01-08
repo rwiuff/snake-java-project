e98751e8f9714b33444fc923aa91df386d435509
Sophie <s235216@student.dtu.dk>
2024-01-08 12:53:15 +0100

    add walls

--- app/src/main/java/snake/Wall.java
+++ app/src/main/java/snake/Wall.java
@@ -1,14 +1,42 @@
 package snake;
 
-public class Wall extends Space {
+import java.util.ArrayList;
+import java.awt.Point;
+import java.util.Random;
+public class Wall extends Space{
 
     public Wall(int x, int y) {
-        super(x, y);
+        super(x,y);
     }
 
     public boolean collision(SnakeObject snake) {
         Main.gameOver((snake.getLength() - 2) * 10);
         return false;
+    }
+    public static void placeWalls(Space[][] spaceArray, ArrayList<Point> emptySpaces) {
+        Random rng = new Random();
+        int amount = rng.nextInt(spaceArray.length*spaceArray[0].length/100*5);
+        for (int i=1; i<=amount;i++) {
+            int index=rng.nextInt(emptySpaces.size());
+            Point coordinates = emptySpaces.get(index);
+            int xCord=(int) coordinates.getX();
+            int yCord= (int) coordinates.getY();
+            boolean legal=true;
+            for (int vertical=xCord-1;vertical<=xCord+1;vertical++) {
+                for (int horizontal=yCord-1;horizontal<=yCord+1;horizontal++) {
+                    if(!(emptySpaces.contains(new Point(vertical,horizontal))) && (vertical!=xCord || horizontal!=yCord)) {
+                        legal=false;
+                        break;
+                    }
+                }
+            }
+            if (legal) {
+                spaceArray[xCord][yCord]=new Wall(xCord, yCord);
+                emptySpaces.remove(coordinates);
+            }
+
+        }
+
     }
 
 }
