761321b6c8f18b9a843d82cf844b744bff52203b
Rasmus Wiuff <rwiuff@gmail.com>
2024-01-11 15:02:33 +0100

    Validates input args

--- app/src/main/java/snake/Main.java
+++ app/src/main/java/snake/Main.java
@@ -11,18 +11,30 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static String[] dimensions;
+    private static int width;
+    private static int height;
 
     public static void main(String[] args) {
-        dimensions = args.length < 2 ? new String[] { "20", "20" } : args;
-        System.out.println("Width: " + dimensions[0]);
-        System.out.println("Height: " + dimensions[1]);
+        String[] input = args.length < 2 ? new String[] { "20", "20" } : args;
+        if (input[0].matches("^[0-9]*[1-9]+$|^[1-9]+[0-9]*$")) {
+            width = Integer.parseInt(input[0]);
+            System.out.println("Width: " + width);
+        } else {
+            width = 20;
+            System.out.println("Invalid width. Width set to: " + width);
+        }
+        if (input[1].matches("^[0-9]*[1-9]+$|^[1-9]+[0-9]*$")) {
+            height = Integer.parseInt(input[1]);
+            System.out.println("Height: " + height);
+        } else {
+            height = 20;
+            System.out.println("Invalid width. Height set to: " + height);
+        }
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        int width = Integer.parseInt(dimensions[0]);
-        int height = Integer.parseInt(dimensions[1]);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Board.fxml"));
         Parent root = loader.load();
         BoardController boardcontroller = loader.getController();
@@ -30,9 +42,9 @@ public class Main extends Application {
         int screenWidth = (int) screenBounds.getWidth();
         int screenHeight = (int) screenBounds.getHeight();
         int fieldSize = 20;
-        if ((width * 20) > screenWidth*0.7 || (height * 20) > screenHeight*0.7) {
-            int heightSize = (int) (screenWidth*0.7 / width);
-            int widthSize = (int) (screenHeight*0.7 / height);
+        if ((width * 20) > screenWidth * 0.7 || (height * 20) > screenHeight * 0.7) {
+            int heightSize = (int) (screenWidth * 0.7 / width);
+            int widthSize = (int) (screenHeight * 0.7 / height);
             fieldSize = height > widthSize ? widthSize : heightSize;
         }
         boardcontroller.setDimensions(width, height, fieldSize);
@@ -44,9 +56,10 @@ public class Main extends Application {
         primaryStage.sizeToScene();
         primaryStage.centerOnScreen();
     }
+
     public static void gameOver(int score) {
         System.out.println("Game Over");
-        System.out.println("Score: "+ score);
+        System.out.println("Score: " + score);
         System.exit(0);
     }
 }
